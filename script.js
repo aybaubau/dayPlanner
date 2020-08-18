4d9b07029408775b6ca51afd4bd186386625b025
Allie Bauer <alliebauer2@gmail.com>
2020-08-17 17:28:23 -0700

    add localStorage component

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ console.log(hour);
 
 $("#currentDay").text(today);
 
-
 $('.description').each(function(){
     if (hour > $(this).attr("id")){
         $(this).attr("class", "description past");
@@ -17,8 +16,20 @@ $('.description').each(function(){
     }
 });
 
-// add event listener for all save btns
-// check in local storage for existing entry
-// create/override existing local storage item
-// render on page
-// add init function to render local storage items on page upon reload
+$('.saveBtn').on('click', function(){
+    var hourKey = $(this).siblings(".description").attr("id");
+    var textValue = $(this).siblings(".description").val();
+    localStorage.setItem(hourKey, textValue);
+})
+
+function init(){
+    if (localStorage.length > 0) {
+        var storageItems = {...localStorage};
+        $.each(storageItems, function(key){
+            var hourID = parseInt(key);
+            $(`#${hourID}`).html(storageItems[hourID]);
+        })
+}
+};
+
+init();
